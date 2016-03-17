2fb7318388530348504046e39dc8f5855a2eebf3
Jinwook Lee <jinwook.lee365@gmail.com>
2016-03-17 01:12:31 -0700

    temp cross-origin

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.use('/', express.static(__dirname + '/public'));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Methods", "POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
