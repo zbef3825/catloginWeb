cb1f138fc78cc5d1e3ed923306b847c511dd1922
Jinwook Lee <jinwook.lee365@gmail.com>
2016-03-17 01:08:10 -0700

    temp cross-origin

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ app = express();
 PORT = process.env.PORT || 3000;
 
 app.use('/', express.static(__dirname + '/public'));
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-}
+});
+
 app.use(expressJWT({secret: "example"}).unless({path: ['/', '/api/idcheck/', '/api/loginCreate/', '/api/login/']}));
 
 dbConnect();
